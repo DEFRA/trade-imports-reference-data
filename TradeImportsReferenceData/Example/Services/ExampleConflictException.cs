b364064db8cdbe12d4cf601b6d8b86b3d3fd5faa
ian.griffiths <ian.griffiths@defra.onmicrosoft.com>
2026-04-28 11:43:26 +0100

    EUDPA-38 - Convert from dotNet project to CDP compliant Java project

--- TradeImportsReferenceData/Example/Services/ExampleConflictException.cs
+++
@@ -1,4 +0,0 @@
-namespace TradeImportsReferenceData.Example.Services;
-
-public sealed class ExampleConflictException(string name, Exception innerException)
-    : Exception($"An example record named '{name}' already exists.", innerException);
